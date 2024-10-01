db16300f286f44da91efe01c8d08f4dd7a797693
haseeb-iqbal <a.haseeb19@gmail.com>
2024-10-01 22:52:41 +0500

    Refactoring

--- script.js
+++ script.js
@@ -1,32 +1,19 @@
-// Get the modal
-var modal = document.getElementById("myModal");
+const profileCard = document.getElementById("profile-card-popup");
+const profileCardCloseButton = document.getElementById("profile-card-close");
+const gridItems = document.getElementsByClassName("grid-item");
 
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on the button, open the modal
-btn.onclick = function () {
-  modal.style.display = "flex";
+profileCardCloseButton.onclick = function () {
+  profileCard.style.display = "none";
 };
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function () {
-  modal.style.display = "none";
-};
-
-// When the user clicks anywhere outside of the modal, close it
 window.onclick = function (event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
+  if (event.target == profileCard) {
+    profileCard.style.display = "none";
   }
 };
 
-const gridItems = document.getElementsByClassName("grid-item");
 for (let i = 0; i < gridItems.length; i++) {
   gridItems[i].onclick = function () {
-    modal.style.display = "flex";
+    profileCard.style.display = "flex";
   };
 }
